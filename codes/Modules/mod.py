37bca49be03e7d1cd29de9c749f94c0e35c67161
KutayOzkoc <kutayozkoc99@gmail.com>
2020-03-22 14:28:44 +0300

    Added New Features !

---
+++ codes/Modules/mod.py
@@ -0,0 +1,23 @@
+"""
+    This is for module.py
+"""
+
+print('Added New Mod.py')
+
+number = 10
+numbers = [1, 2, 3]
+person = {
+    'name': 'David',
+    'age': '25',
+    'city': 'NewYork'
+
+}
+
+
+def func(x):
+    print(f'x:{x}')
+
+
+class Person():
+    def speak(self):
+        print('HELLO')
